08563439843e0ec876126f473ca174eed8010cdf
Rowine Mabiog <rowinemabiog24@gmail.com>
2020-05-24 08:48:28 +0800

    Complete side bar function

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-const getElement = (el) => {
-    document.querySelector(el);
-};
+const selectElement = (el) => document.querySelector(el)
+;
 
+//Open menu on click
+
+selectElement('.fa-bars').addEventListener('click', () => {
+    selectElement('.navigation__list').classList.add('active');
+});
+
+//Close menu on click
+
+selectElement('.fa-times').addEventListener('click', () => {
+    selectElement('.navigation__list').classList.remove('active');
+});
